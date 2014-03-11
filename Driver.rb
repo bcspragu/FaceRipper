242a2edceb5a7423e5a1678e0214ee340c6250ff
Brandon Sprague <brandon00sprague@gmail.com>
2014-03-10 21:35:44 -0400

    Welp, it works

--- Driver.rb
+++ Driver.rb
@@ -8,38 +8,31 @@ facebook_config = YAML::load(File.open('config/facebook.yml'))
 access_token = facebook_config['access_token']
 user_id = facebook_config['user_id']
 
-url = "https://graph.facebook.com/#{user_id}?access_token=#{access_token}&fields=id,name,friends.limit(1).fields(name,statuses.limit(50).fields(likes,message))";
+friend_query = "https://graph.facebook.com/#{user_id}?access_token=#{access_token}&fields=id,name,friends.fields(name,id)"
 
-content = JSON.parse(open(url).read)
+query = JSON.parse(open(friend_query).read)
 
-user = User.where(name: content['name'], facebook_id: content['id']).first_or_create
+load_friends(query['friends'])
 
-friend_json = content['friends']['data'].first
+user = User.where(name: query['name'], facebook_id: query['id']).first_or_create
 
-get_statuses(friend_json)
-
-friend_json = content['friends']
-while friend_json['paging'] and friend_json['paging']['next']
-    url = friend_json['paging']['next']
-    content = JSON.parse(open(url).read)
-    friend_json = content
-    p friend_json
-    get_statuses(friend_json['data'].first)
+User.all.each do |user|
+  status_query = "https://graph.facebook.com/#{user_id}?access_token=#{access_token}&fields=id,name,friends.uid(#{user.facebook_id}).fields(name,statuses)"
+  get_statuses(user,JSON.parse(open(status_query).read)['friends']['data'].first)
 end
-
 BEGIN {
 def add_status(friend,status_data)
   status = Status.create(message: status_data['message'], user_id: friend.id)
   unless status_data['likes'].nil?
     status_data['likes']['data'].each do |like_data|
-      liker = User.where(name: like_data['name'], facebook_id: like_data['id']).first_or_create
-      status.likes.create(user_id: liker.id)
+      liker = User.where(name: like_data['name'], facebook_id: like_data['id']).first
+      status.likes.create(user_id: liker.id) if liker
     end
   end
 end
 
-def get_statuses(user_json)
-  friend = User.where(name: user_json['name'], facebook_id: user_json['id']).first_or_create
+def get_statuses(friend,user_json)
+  return unless user_json['statuses'] and user_json['statuses']['data']
   user_json['statuses']['data'].each do |status_data|
     add_status(friend,status_data)
   end
@@ -51,6 +44,20 @@ def get_statuses(user_json)
       add_status(friend,status_data)
     end
     status_info = content
+  end
+end
+
+def load_friends(friend_json)
+  friends_from_data(friend_json['data'])
+  while friend_json['paging'] and friend_json['paging']['next']
+    friend_json = JSON.parse(open(friend_json['paging']['next']).read)
+    friends_from_data(friend_json['data'])
+  end
+end
+
+def friends_from_data(data)
+  data.each do |user_json|
+    User.where(name: user_json['name'], facebook_id: user_json['id']).first_or_create
   end
 end
 }
